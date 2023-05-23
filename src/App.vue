9496a170cdab3fb307a52eea751074176a1d38b3
Balazs Makrai <b.makrai@gmail.com>
2023-05-23 13:01:36 +0200

    add home submenu

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@
         </button>
       </div>
       <button
+        @click="toggleMenu"
+        :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
         class="rounded-full bg-black hover:bg-[#282828] p-0.5 mr-8 mt-0.5 cursor-pointer"
       >
         <div class="flex items-center">
@@ -31,17 +33,40 @@
           </div>
           <ChevronDown
             v-if="!openMenu"
-            @click="openMenu = true"
-            fillColor="#FFFFFF"
-            :size="25"
-          /> <ChevronUp
-            v-else
-            @click="openMenu = false"
+            @click="toggleMenu"
             fillColor="#FFFFFF"
             :size="25"
           />
-        </div>
-      </button>
+          <ChevronUp
+            v-else
+            @click="toggleMenu"
+            fillColor="#FFFFFF"
+            :size="25"
+          />
+        </div></button
+      ><span
+        v-if="openMenu"
+        class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer"
+      >
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li
+            class="px-3 py-2.5 hover:bog-[#3E3D3D] border-b border-b-gray-600"
+          >
+            Profile
+          </li>
+          <li class="px-3 py-2.5 hover:bog-[#3E3D3D]">Log out</li>
+        </ul></span
+      >
+    </div>
+
+    <div id="SideNav" class="p-6 h-[100%] w-[240px] fixed z-50 bg-black">
+      <RouterLink to="/">
+        <img src="/images/icons/spotify-logo.png" width="125" />
+      </RouterLink>
+      <div class="my-8"></div>
+      <RouterLink to="/">
+        <MenuItem class="ml-[1px]"  name="Home" iconString="home" pageUrl="/" :iconSize="23"/>
+      </RouterLink>
     </div>
   </div>
 </template>
@@ -52,6 +77,9 @@ import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
 import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
 import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
 import ChevronLeft from "vue-material-design-icons/Chevronleft.vue";
-
+import MenuItem from "./components/MenuItem.vue";
 const openMenu = ref(false);
+const toggleMenu = () => {
+  openMenu.value = !openMenu.value;
+};
 </script>
