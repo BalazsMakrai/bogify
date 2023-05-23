fd01db819601f5077289e282385cba2b6b58ceb8
Balazs Makrai <b.makrai@gmail.com>
2023-05-23 11:18:07 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div
+      class="w-[calc(100%-240px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
+    >
+      <div class="items-center ml-6 flex">
+        <button
+          type="button"
+          class="rounded-full bg-black p-[1px] cursor-pointer"
+        >
+          <ChevronLeft fillColor="#FFFFFF" :size="30" />
+        </button>
+        <button
+          type="button"
+          class="rounded-full bg-black p-[1px] cursor-pointer ml-4"
+        >
+          <ChevronRight fillColor="#FFFFFF" :size="30" />
+        </button>
+      </div>
+      <button
+        class="rounded-full bg-black hover:bg-[#282828] p-0.5 mr-8 mt-0.5 cursor-pointer"
+      >
+        <div class="flex items-center">
+          <img
+            class="rounded-full"
+            src="https://yt3.ggpht.com/yti/AHyvSCC087C8Qmyp3f5T2tKz1WJOqYjpVZeUJ8U0gkwA4w=s88-c-k-c0x00ffffff-no-rj-mo"
+            width="27"
+          />
+          <div class="text-white text-[14px] ml-1.5 font-semibold">
+            Makrai Balázs
+          </div>
+          <ChevronDown
+            v-if="!openMenu"
+            @click="openMenu = true"
+            fillColor="#FFFFFF"
+            :size="25"
+          /> <ChevronUp
+            v-else
+            @click="openMenu = false"
+            fillColor="#FFFFFF"
+            :size="25"
+          />
+        </div>
+      </button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
+import ChevronLeft from "vue-material-design-icons/Chevronleft.vue";
+
+const openMenu = ref(false);
+</script>
